16820d873a078dfd179fdc75a9a4f71dffb7be19
Maciej Mionskowski <business@maciekmm.net>
2016-01-30 19:48:05 +0100

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+var CACHE = 'v1';
+var API_HOST = 'api.vlo.maciekmm.net';
+
+this.addEventListener('fetch', function(event) {
+  var url = new URL(event.request.url);
+
+  if (url.hostname === API_HOST) {
+    event.respondWith(
+      fetch(event.request)
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if(response) {
+        	return response;
+        }
+        return fetch(event.request).then(function(response) {
+          return caches.open(CACHE).then(function(cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  }
+});
